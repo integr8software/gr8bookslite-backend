a4ae8023810729476d3464a6b959e32b567d04c3
integr8-dev-jeshua <jbay@integr8.com.ph>
2026-06-18 15:53:11 +0800

    FIX: From Start Migration and Seeds of Address

---
+++ prisma/migrations/20260618074053/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "psgc_barangays" ALTER COLUMN "updated_at" DROP DEFAULT;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,12 @@
 import { assertLocalDatabase } from './scripts/assertLocalDatabase';
 import { runSeedTask } from './scripts/runSeedTask';
+import { seedAddressReferences } from './seeds/seedAddressReferences';
 import { seedLocalFixtures } from './seeds/seedLocalFixtures';
 import { seedSubscriptionPlans } from './seeds/seedSubscriptionPlans';
 
 void runSeedTask('Local Prisma seed', async () => {
   assertLocalDatabase();
+  await seedAddressReferences();
   await seedSubscriptionPlans();
   await seedLocalFixtures();
 });
